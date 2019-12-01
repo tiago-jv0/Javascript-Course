77f6ccc541e43ad1725f1267d0b2d53e0408e7b4
Tiago José <tiago_jvo@outlook.com>
2019-12-01 13:08:06 -0300

    Fundamentals 1

---
+++ Fundamentos/parNomeValor.js
@@ -0,0 +1,6 @@
+const saudacao = 'Hello' //Contexto léxico 1
+
+function exec(){
+    const saudacao = 'Hello' // Contexto léxico 2
+    return saudacao
+}
---
+++ Fundamentos/usandoLetEmLoop1.js
@@ -0,0 +1,5 @@
+for (let number = 0; number < 10; number++) {
+    console.log(number);
+}
+
+console.log(number)
